d3c55e9e5dc1dc60acf19d8c6b0f22b14488b9fb
marek <marek-schuetze@mail.de>
2022-10-12 18:06:06 +0200

    BE: Add Integration Test for getAllCommands and postNewCommand

--- backend/src/test/java/com/example/backend/controller/MovieControllerTest.java
+++ backend/src/test/java/com/example/backend/controller/MovieControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.http.HttpHeaders;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
